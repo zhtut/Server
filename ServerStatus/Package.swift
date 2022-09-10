1065d5d76738404ba1ce450d6c59a73d650d77e7
周途广 <zhtg@icloud.com>
2022-09-10 18:32:43 +0800

    feat: 更新可运行名称

--- ServerStatus/Package.swift
+++ ServerStatus/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
-        .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
+        .executableTarget(name: "ServerStatus", dependencies: [.target(name: "App")]),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
