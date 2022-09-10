53b77355a268d467b24f68dc774ec61bce800b16
周途广 <zhtg@icloud.com>
2022-09-10 20:44:57 +0800

    feat: 修改安装

--- Shell/install_postgres.sh
+++ Shell/install_postgres.sh
@@ -1,6 +1,6 @@
 sudo apt install postgresql postgresql-contrib
 
-sudo -u postgres psql -c "SELECT version();"
+sudo su postgres -c "SELECT version();"
 
 echo "允许远程访问"
 echo "修改监听地址"
@@ -14,6 +14,6 @@ echo "修改可访问的ip段"
 export host="host all all 0.0.0.0/0 md5"
 export host_ipv6="host all all ::0/0 md5"
 export pg_hba_conf="/etc/postgresql/*/main/pg_hba.conf"
-sudo echo ${host} >>${pg_hba_conf}
-sudo echo ${host_ipv6} >>${pg_hba_conf}
+echo "${host}" | sudo tee -a ${pg_hba_conf}
+echo "${host_ipv6}" | sudo tee -a ${pg_hba_conf}
 sudo cat ${pg_hba_conf}
